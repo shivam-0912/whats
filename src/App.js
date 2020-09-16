cd8011794de68d842172c70c1fbdc6bdd4550d32
shivam-0912 <shivamtayal1992@gmail.com>
2020-09-16 14:00:05 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component{
     key:'',
     phone:'',
     message:'',
-    submitted:false
+    submitted:false,
+    data: "hv"
   }
   postDataHandler = () => {
     const data = {
@@ -24,11 +25,33 @@ class App extends Component{
               key:'',
               phone:'',
               message:'',
-              submitted:false
+              submitted:false,
+              
             } );
         });
       
 }
+componentDidMount() {
+  // Call our fetch function below once the component mounts
+
+this.callBackendAPI()
+  .then(res => {
+    console.log(res.express)
+    return this.setState({ data: res.express })
+  })
+  .catch(err => console.log(err));
+}
+// Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+callBackendAPI = async () => {
+const response = await fetch('/express_backend');
+const body = await response.json();
+
+if (response.status !== 200) {
+  throw Error(body.message) 
+}
+return body;
+};
+
 render () {
  
   return (
